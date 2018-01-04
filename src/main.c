8a75416eec1309846c74d9c3575b017995c11bc8
PostHocErgoPropterHoc <daneel.francois@gmail.com>
2018-01-05 00:56:56 +0100

    SDL functionnal and clicks registering

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 // constante
 #define WIDTH_SCREEN 800
-#define HEIGHT_SCREEN 600
+//#define HEIGHT_SCREEN 600
 
 int main(int argc, char **argv){
 
@@ -15,9 +15,9 @@ int main(int argc, char **argv){
     SDL_Renderer *renderer = NULL;
     SDL_Event ev;
     int continuer = 1;
-    char file_name[100], filecopy_name[100];
+    char file_name[120], filecopy_name[120], name_for_texture[120];
     int bitmap, reso_vert, reso_hor, pix_format, pix_offset, image_size, data_size;
-    int data, i;
+    int i;
     FILE *fp, *gauss_blur, *sobel;
 
     do{
@@ -36,6 +36,7 @@ int main(int argc, char **argv){
         printf("Fichier %s ouvert\n", file_name);
     }
 
+    strncpy(name_for_texture, file_name, sizeof(file_name));
     bitmap = bm_check(fp);
     if(bitmap == 1){
         printf("Le fichier est bien un fichier bitmap\n");
@@ -59,6 +60,60 @@ int main(int argc, char **argv){
             printf("Format pixel non supporté\n");
             return EXIT_FAILURE;
         }
+
+        //SDL2 to display the original image and take inputs
+        //SDL2 variables
+        int HEIGHT_SCREEN = (int)(WIDTH_SCREEN * (reso_vert/(double)reso_hor));//scale the window to the image
+        int mouse_click = 0, x_start, y_start, x_finish, y_finish;
+        double x_ratio, y_ratio;
+
+        if(SDL_Init(SDL_INIT_VIDEO) != 0){
+            fprintf(stdout, "echec init sdl %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+        }
+
+        fenetre = SDL_CreateWindow(
+            "PathFinder -- FRANCOIS DAMIEN --",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            WIDTH_SCREEN, HEIGHT_SCREEN,0
+        );
+
+        renderer = SDL_CreateRenderer(fenetre, -1,
+            SDL_RENDERER_ACCELERATED
+        );
+        SDL_Surface * image = SDL_LoadBMP(name_for_texture);
+        SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+
+        while((continuer)&&(mouse_click != 2)){
+            while(SDL_PollEvent(&ev)){
+                switch(ev.type){
+                    case SDL_QUIT:
+                        continuer = 0;
+                        break;
+                    case SDL_MOUSEBUTTONDOWN://two clicks to register coordinates
+                        if(mouse_click == 0){
+                            SDL_GetMouseState(&x_start, &y_start);
+                            mouse_click++;
+                        }
+                        else{
+                            SDL_GetMouseState(&x_finish, &y_finish);
+                            mouse_click++;
+                        }
+                        break;
+                }
+            }
+        }
+
+        //calculate ratio window resolution to image resolution and pixel position on image
+        x_ratio = (reso_hor/(double)WIDTH_SCREEN);
+        y_ratio = (reso_vert/(double)HEIGHT_SCREEN);
+        x_start = (int)(x_start * x_ratio);
+        y_start = (int)(y_start * y_ratio);
+        x_finish = (int)(x_finish * x_ratio);
+        y_finish = (int)(y_finish * y_ratio);
 
         if(bm_grayscale_check(fp) == 1){
             printf("* Grayscale\n");
@@ -94,32 +149,10 @@ int main(int argc, char **argv){
 
         sobel_filter(sobel, file_name, filecopy_name);
 
-        /*if(SDL_Init(SDL_INIT_VIDEO) != 0){
-        fprintf(stdout, "echec init sdl %s\n", SDL_GetError());
-        return EXIT_FAILURE;
-        }
-
-        fenetre = SDL_CreateWindow(
-            "PathFinder -- FRANCOIS DAMIEN --",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            WIDTH_SCREEN, HEIGHT_SCREEN,0
-        );
-
-        renderer = SDL_CreateRenderer(fenetre, -1,
-            SDL_RENDERER_ACCELERATED
-        );
-
-        while(continuer){
-            SDL_WaitEvent(&ev);
-            switch(ev.type){
-                case SDL_QUIT:
-                    continuer = 0;
-                    break;
-            }
-        }*/
         fclose(sobel);
         fclose(gauss_blur);
+
+        printf("* Done!\n");
     }
     else if(bitmap == 0){
         printf("* Le fichier n'est pas un fichier bitmap\n");
